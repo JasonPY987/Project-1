a2e8da276d5aa1026057f9be144141b4628e8c37
JasonPY987 <corey.business.987@outlook.com>
2022-11-28 17:36:56 +0400

    second commit

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,9 @@
+import random
+import markdown2
+from django import forms 
+from django.shortcuts import render, redirect
+
+
 from django.shortcuts import render
 
 from . import util
@@ -8,3 +14,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+    markdowner = Markdown(entry)
+    entryPage = util.get_entry(entry)
+    if entryPage is none:
+        return render(request, "encyclopedia/NoEntryHere.html", {
+           "entryTitle": entry
+        })
+    else:
+        return render(request,"encyclopedia/entry.html", {
+            "entry": markdowner.convert(entryPage),
+            "entryTitle": entry
+        })
