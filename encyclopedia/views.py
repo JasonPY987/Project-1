3f4b5044bf841c0420ea412ce57c0b62d07771c1
JasonPY987 <corey.business.987@outlook.com>
2022-11-29 17:04:45 +0400

    second commit on second day creating TODO list

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,18 @@
 import random
-import markdown2
-from django import forms 
+from django import forms
 from django.shortcuts import render, redirect
-
-
+from markdown2 import Markdown
 from django.shortcuts import render
-
 from . import util
+
+
+def convert_MKDWN_to_HTML(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
 
 
 def index(request):
@@ -14,15 +20,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
 def entry(request, entry):
     markdowner = Markdown(entry)
     entryPage = util.get_entry(entry)
     if entryPage is none:
         return render(request, "encyclopedia/NoEntryHere.html", {
-           "entryTitle": entry
+            "entryTitle": entry
         })
     else:
-        return render(request,"encyclopedia/entry.html", {
+        return render(request, "encyclopedia/entry.html", {
             "entry": markdowner.convert(entryPage),
             "entryTitle": entry
         })
